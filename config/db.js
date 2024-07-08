e1885279efcf84e2a2fb310625000dc5b9ad6211
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-08 20:22:59 +0530

    router setup

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("connect to DB")
+        // console.log("connect to DB")
     } catch (error) {
         console.log(err);
     }
