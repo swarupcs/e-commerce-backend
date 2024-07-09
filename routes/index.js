6d4e7f9c1adafb18da547e471092d2d79e4e5fb6
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-09 17:31:21 +0530

    logout functionality

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ const userSignUpController = require("../controller/userSignUp");
 const userSignInController = require('../controller/userSignIn');
 const userDetailsController = require('../controller/userDetails');
 const authToken = require('../middleware/authToken');
+const userLogout = require('../controller/userLogout');
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
 router.get("/user-details", authToken,userDetailsController);
+router.get("/userlogout", userLogout);
 
 module.exports = router
