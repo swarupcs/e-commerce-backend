6b8855b06d37152f81385a2f0b7463abc0e1ac18
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-12 21:58:53 +0530

    category wise product fetched

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,6 @@ const express = require('express');
 
 const router = express.Router()
 
-// const userSignUpController = require("../controller/user/userSignUp");
-// const userSignInController = require('../controller/user/userSignIn');
-// const userDetailsController = require('../controller/user/userDetails');
-// const authToken = require('../middleware/authToken');
-// const userLogout = require('../controller/user/userLogout');
-// const allUser = require('../controller/user/allUser');
-// const updateUser = require('../controller/user/updateUser');
-// const UploadController = require('../controller/product/uploadProduct');
-// const getProductController = require('../controller/product/getProduct');
-// const updateProductController = require('../controller/product/updateProduct');
-
 const userSignUpController = require("../controller/user/userSignUp");
 const userSignInController = require("../controller/user/userSignIn");
 const userDetailsController = require("../controller/user/userDetails");
@@ -23,6 +12,7 @@ const updateUser = require('../controller/user/updateUser');
 const UploadController = require("../controller/product/uploadProduct");
 const getProductController = require("../controller/product/getProduct");
 const updateProductController = require("../controller/product/updateProduct");
+const getCategoryProduct = require('../controller/product/getCategoryProduct');
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -38,6 +28,7 @@ router.post("/update-user", authToken, updateUser)
 router.post("/upload-product", authToken, UploadController);
 router.get("/get-product", getProductController);
 router.post("/update-product", authToken, updateProductController);
+router.get("/get-categoryProduct", getCategoryProduct);
 
 
 module.exports = router
