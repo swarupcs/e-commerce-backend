a900ebdabd92a2de821b31cf42e64f3e858d965e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-12 10:24:50 +0530

    upload product backend design

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const authToken = require('../middleware/authToken');
 const userLogout = require('../controller/userLogout');
 const allUser = require('../controller/allUser');
 const updateUser = require('../controller/updateUser');
+const UploadController = require('../controller/uploadProduct');
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -19,5 +20,9 @@ router.get("/userlogout", userLogout);
 //admin panel
 router.get('/all-user', authToken, allUser);
 router.post("/update-user", authToken, updateUser)
+
+//product
+router.post("/upload-product", authToken, UploadController);
+
 
 module.exports = router
