af0c9a55197d7df9113e819019799efbaccf812c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-07-13 20:18:21 +0530

    getCategorywise product backend design

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ const UploadController = require("../controller/product/uploadProduct");
 const getProductController = require("../controller/product/getProduct");
 const updateProductController = require("../controller/product/updateProduct");
 const getCategoryProduct = require('../controller/product/getCategoryProductOne');
+const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct');
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -29,6 +30,7 @@ router.post("/upload-product", authToken, UploadController);
 router.get("/get-product", getProductController);
 router.post("/update-product", authToken, updateProductController);
 router.get("/get-categoryProduct", getCategoryProduct);
+router.post("/category-product", getCategoryWiseProduct);
 
 
 module.exports = router
